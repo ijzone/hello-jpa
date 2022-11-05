3528a0a0c2ebc330d6a34e6f6ab0a7f6c056fced
x1-windows10 <ijzone.dev@gmail.com>
2022-11-05 18:07:28 +0900

    4-3. 인프런 - 자바 ORM 표준 JPA 프로그래밍 - 기본편 _ 필드와 컬럼 매핑

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -15,17 +15,6 @@ public class JpaMain {
         tx.begin();
 
         try {
-            // 영속
-            Member member = em.find(Member.class, 150L);
-            member.setName("AAAAA");
-
-            em.detach(member);  // 영속성 컨텍스트에서 더이상 관리하지 않는다.
-//            em.clear(); // 영속성 컨텍스트 초기화
-            Member member2 = em.find(Member.class, 150L);
-//            System.out.println(member == member2);  // 초기화 이후 DB에서 다시 조회하기 때문에 false
-
-            System.out.println("=====================");
-
             tx.commit();    // 영속성 컨텍스트가 DB에 저장되는 시점
         } catch (Exception e) {
             tx.rollback();
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,38 +1,44 @@
 package hellojpa;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
 
 @Entity
 //@Table(name = "MBR")
 public class Member {
 
     @Id
-    private long id;
-    private String name;
+    private Long id;
+
+    @Column(name = "name")
+    private String username;
+
+    private Integer age;
+
+    @Enumerated(EnumType.STRING)    // 기본값인 Ordinal 사용 X!
+    private RoleType roleType;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+
+    // LocalDate & LocalDateTime은 annotation 없어도 JPA에서 date 타입으로 인식한다.
+    private LocalDate testLocalDate;
+    private LocalDateTime testLocalDateTime;
+
+    @Lob
+    private String description;
+
+    // 테이블과 매핑하지 않는 컬럼에 붙인다.
+    @Transient
+    private int temp;
 
     public Member() {
     }
 
-    public Member(long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
 
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
