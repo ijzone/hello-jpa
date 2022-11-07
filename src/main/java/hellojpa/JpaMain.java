e751f3425e756fdd524b07db6285877ed037e152
x1-windows10 <ijzone.dev@gmail.com>
2022-11-07 19:24:19 +0900

    5-1. 인프런 - 자바 ORM 표준 JPA 프로그래밍 - 기본편 _ 단방향 연관관계

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -15,6 +15,25 @@ public class JpaMain {
         tx.begin();
 
         try {
+            // 저장
+            Team team = new Team();
+            team.setName("TeamA");
+            em.persist(team);
+
+            Member member = new Member();
+            member.setUsername("member1");
+//            member.setTeamId(team.getId());
+            member.setTeam(team);   // 객체 연관관계 매핑
+            em.persist(member);
+
+            Member findMember = em.find(Member.class, member.getId());
+//            Long findTeamId = findMember.getTeamId();
+//            Team findTeam = em.find(Team.class, findTeamId);
+            Team findTeam = findMember.getTeam();   // 객체 연관관계 매핑
+            System.out.println("findTeam = " + findTeam.getName());
+
+
+
             tx.commit();    // 영속성 컨텍스트가 DB에 저장되는 시점
         } catch (Exception e) {
             tx.rollback();
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -6,17 +6,44 @@ import java.time.LocalDateTime;
 import java.util.Date;
 
 @Entity
-//@Table(name = "MBR")
 public class Member {
 
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id @GeneratedValue
+    @Column(name = "MEMBER_ID")
     private Long id;
 
-    @Column(name = "name", nullable = false)
+    @Column(name = "USERNAME")
     private String username;
 
-    public Member() {
+//    @Column(name = "TEAM_ID")
+//    private Long teamId;
+
+    // 객체 연관관계 매핑
+    @ManyToOne  // N:1
+    @JoinColumn(name = "TEAM_ID")   // 조인해야 할 컬럼명
+    private Team team;
+
+    public Long getId() {
+        return id;
     }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
 
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
+    }
 }
