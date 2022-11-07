f4897391cc67bdbe2b9d862a7f7fd4d2f7dc1856
x1-windows10 <ijzone.dev@gmail.com>
2022-11-07 18:38:28 +0900

    4-5. 인프런 - 자바 ORM 표준 JPA 프로그래밍 - 기본편 _ 실전 예제 1 - 요구사항 분석과 기본 매핑

---
+++ jpashop/src/main/java/jpabook/jpashop/JpaMain.java
@@ -0,0 +1,25 @@
+package jpabook.jpashop;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();   // DB 작업은 꼭 EntityManager를 통해서 해야된다.
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            tx.commit();    // 영속성 컨텍스트가 DB에 저장되는 시점
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+        emf.close();
+    }
+}
